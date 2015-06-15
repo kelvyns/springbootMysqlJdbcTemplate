d8ca8df6214ea29e87f6bc793a32526b43ef1f7d
System Administrator <root@MacBook-Air-de-Kelvyns.local>
2015-06-15 20:45:02 -0300

    create springboot project with jdbcTemplate and mysql

---
+++ src/test/java/jdbcTemplate/JdbcTemplateApplicationTests.java
@@ -0,0 +1,16 @@
+package jdbcTemplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JdbcTemplateApplication.class)
+public class JdbcTemplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
